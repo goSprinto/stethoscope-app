ccebf1feb3141eb205f769e60fb50796239e18ad
Abhaya <abhaya@sprinto.com>
2021-05-10 22:02:57 +0530

    Use tryExec to handle missing preferences

--- src/sources/darwin/screen-lock.sh
+++ src/sources/darwin/screen-lock.sh
@@ -17,7 +17,7 @@ save screen.lockDelay
 
 remove output
 
-exec defaults -currentHost read com.apple.screensaver
+tryExec defaults -currentHost read com.apple.screensaver
 extract idleTime\s*=\s*(\d*);
 defaultTo -1
 save screen.idleDelay
