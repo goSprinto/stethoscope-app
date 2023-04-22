51a926ecf8d2aa411ffaa212eb36a688e926028f
Manoj Jadhav <manoj@sprinto.com>
2023-04-22 09:28:21 +0530

    Fix format

--- src/sources/win32/screensaver-policy.sh
+++ src/sources/win32/screensaver-policy.sh
@@ -1,19 +1,19 @@
 # tryExec powershell 'Get-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Control Panel\Desktop\" | Format-List'
-tryExec reg query 'HKCU\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop'
+exec reg query 'HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop'
 trim
 save output1
 
-extract ScreenSaveActive\s+:\s+([0-9]*)
+extract ScreenSaveActive\s+REG_SZ\s+([0-9]*)
 defaultTo -1
 save screenSaveActive
 
 load output1
-extract ScreenSaverIsSecure\s+:\s+([0-9]*)
+extract ScreenSaverIsSecure\s+REG_SZ\s+([0-9]*)
 defaultTo -1
 save screenSaverIsSecure
 
 load output1
-extract ScreenSaveTimeout\s+:\s+([0-9]*)
+extract ScreenSaveTimeout\s+REG_SZ\s+([0-9]*)
 defaultTo -1
 save screenSaveTimeout
 
