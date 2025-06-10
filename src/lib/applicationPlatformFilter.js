01f0a795ae087e9917b58f48a61c2854da9829a2
Manoj Jadhav <manojjadhav@Manojs-MacBook-Pro.local>
2025-06-10 16:08:58 +0530

    handled undefined distroId

--- src/lib/applicationPlatformFilter.js
+++ src/lib/applicationPlatformFilter.js
@@ -4,18 +4,6 @@ import kmd from './kmd'
 // elements appropriate for the running OS platform/version
 export default async function applicationPlatformFilter (applications = [], context, platform, version) {
   const osPlatform = platform || process.platform
-  let osVersion 
-  if(version){
-    osVersion = version
-  } else {
-    const result = await kmd('os', context)
-    const distroId = result.system.distroId;
-    if (distroId == "debian") {
-      osVersion = result.system.debian_version;
-    }else{
-      osVersion = result.system.version || result.system.lsb_version;
-    }
-  }
 
   // Note: removed OS version check
   return applications.filter((app) => {
