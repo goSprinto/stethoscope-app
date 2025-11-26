daeb8d48e29e5c2e29376ecfad64c789834344ed
Nisarg Shah <nisargs@sprinto.com>
2025-11-26 16:54:41 +0530

    fix(dr.sprinto): mac hostname issue fix

--- src/sources/darwin/hostname.sh
+++ src/sources/darwin/hostname.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env kmd
-exec hostname
+# Fallback to LocalHostName if ComputerName is not set
+exec sh -c 'scutil --get ComputerName 2>/dev/null || scutil --get LocalHostName 2>/dev/null || hostname -s 2>/dev/null || hostname'
 save system.hostname
